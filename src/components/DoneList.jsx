9ec76e65e89ce0f8864456f7b1dc6f479110d845
shun <ringorian0626@gmail.com>
2019-12-15 16:36:07 +0900

    fix style

--- src/components/DoneList.jsx
+++ src/components/DoneList.jsx
@@ -17,8 +17,7 @@ const DoneList = (props) => (
             isDisplayDate = true
           }
        }
-       console.log(isDisplayDate)
-       return <div key={i}>{isDisplayDate && <h3 className="siimple--color-white">{todo.date}</h3>}<div key={i} className="siimple-list-item siimple--bg-white fs10">{todo.title} <span className="siimple-tag siimple-tag--error siimple-hover" style={btn} onClick={() => props.handleRemove(i)}>Delete</span></div></div>
+       return <div key={i}>{isDisplayDate && <h3 className="siimple--color-white">{todo.date}</h3>}<div key={i} className="siimple-list-item siimple--bg-white fs10 btlr5 btrr5 m-bottom-10">{todo.title} <span className="siimple-tag siimple-tag--error siimple-hover" style={btn} onClick={() => props.handleRemove(i)}>Delete</span></div></div>
      })}
    </div>
 );
