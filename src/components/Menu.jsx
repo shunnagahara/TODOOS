840b7f8c44b8a8b12e97c229d5ae1906ea6d79a5
shun <ringorian0626@gmail.com>
2019-12-15 15:18:00 +0900

    fixed URL

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -17,32 +17,18 @@ let style = { textDecoration: 'none', color: "#fff" };
 function Headers() {
   return (
   <div className="siimple-navbar">
-      {/* <div className="siimple--float-left">
-        <div className="siimple-btn siimple-btn--red m-right-5">
-          <Link to = "/" style={style}>TOP</Link>
-        </div>
-        <div className="siimple-btn siimple-btn--orange m-right-5">
-          <Link to = "/work" style={style}>Work</Link>
-        </div>
-        <div className="siimple-btn siimple-btn--yellow m-right-5">
-          <Link to = "/private" style={style}>Private</Link>
-        </div>
-        <div className="siimple-btn siimple-btn--green">
-          <Link to = "/done" style={style}>Done</Link>
-        </div>
-      </div> */}
       <div className="siimple--float-left">
         <div className="siimple-btn siimple-btn--red m-right-5 fs12">
-          <Link to = "/" style={style}>TOP</Link>
+          <Link to = "/TODOOS" style={style}>TOP</Link>
         </div>
         <div className="siimple-btn siimple-btn--orange m-right-5 fs12">
-          <Link to = "/work" style={style}>Work</Link>
+          <Link to = "/TODOOS/work" style={style}>Work</Link>
         </div>
         <div className="siimple-btn siimple-btn--yellow m-right-5 fs12">
-          <Link to = "/private" style={style}>Private</Link>
+          <Link to = "/TODOOS/private" style={style}>Private</Link>
         </div>
         <div className="siimple-btn siimple-btn--green fs12">
-          <Link to = "/done" style={style}>Done</Link>
+          <Link to = "/TODOOS/done" style={style}>Done</Link>
         </div>
       </div>
   </div>
@@ -55,10 +41,10 @@ export default class Menu extends Component {
       <Router>
         <div>
           {Headers()}
-          <Route path="/" exact component={Top}/>
-          <Route path="/work" component={Work}/>
-          <Route path="/private" component={Private}/>
-          <Route path="/done" component={Done}/>
+          <Route path="/TODOOS" exact component={Top}/>
+          <Route path="/TODOOS/work" component={Work}/>
+          <Route path="/TODOOS/private" component={Private}/>
+          <Route path="/TODOOS/done" component={Done}/>
         </div>
       </Router>
     );
